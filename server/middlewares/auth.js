a50fe5dfd71d2fd4af4276bc4f1df4cbce6679ca
MohitSaraswat17 <145154965+MohitSaraswat17@users.noreply.github.com>
2024-06-17 12:14:48 +0530

    Little problem fixed

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,7 +1,7 @@
 import { User } from "../models/userSchema.js";
 import ErrorHandler from "./error.js";
 import jwt from 'jsonwebtoken';
-import {} from "../models/userSchema.js"
+import {User} from "../models/userSchema.js"
 import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js"
 
 export const isAuthenticated = catchAsyncErrors(async(req,res,next)=>{
